77d9c316618518ca2882092b5974b5c6ef128d36
Edgar Castro <edzed402@gmail.com>
2015-09-30 00:11:12 -0400

    Made new scene with Chris' art work. Level has colliders keeping the player wolf in.

--- Assets/Scripts/Howl Stage Scripts/Current Stage Scripts/WolfParticleToDen.cs
+++ Assets/Scripts/Howl Stage Scripts/Current Stage Scripts/WolfParticleToDen.cs
@@ -2,23 +2,19 @@
 using System.Collections;
 
 public class WolfParticleToDen : MonoBehaviour {
-	ParticleSystem particleToDen;
 
-	ParticleEmitter emitter;
-	GameObject Den;
-	Vector2 DenPoint;
+	GameObject den;
+	GameObject particleToDen;
 
 	// Use this for initialization
 	void Start () {
-		emitter = GetComponent<ParticleEmitter> ();
-		particleToDen = GetComponent<ParticleSystem> ();
-		Den = GameObject.Find("WolfDen");
-		DenPoint = Den.transform.position;
+		particleToDen = GameObject.Find("Particle To Den");
+		den = GameObject.Find("WolfDen");
 	}
+
 	
 	// Update is called once per frame
 	void Update () {
-		//emitter.Emit(Vector3.zero, DenPoint, 0.2, 2, Color.yellow);
-		//particleToDen.
+		transform.rotation = Quaternion.Euler( Mathf.Atan2(den.transform.position.y - transform.position.y, den.transform.position.x - transform.position.x) * Mathf.Rad2Deg,270, 270);
 	}
 }
