835d8094d0724900c30f4b04e1aeeb121231724a
Edgar Castro <edzed402@gmail.com>
2015-09-29 20:34:18 -0400

    Trying to get particles to move towards Den. Just uploading progress so far to pull most recent commit.

---
+++ Assets/Scripts/Howl Stage Scripts/Current Stage Scripts/WolfParticleToDen.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class WolfParticleToDen : MonoBehaviour {
+	ParticleSystem particleToDen;
+
+	ParticleEmitter emitter;
+	GameObject Den;
+	Vector2 DenPoint;
+
+	// Use this for initialization
+	void Start () {
+		emitter = GetComponent<ParticleEmitter> ();
+		particleToDen = GetComponent<ParticleSystem> ();
+		Den = GameObject.Find("WolfDen");
+		DenPoint = Den.transform.position;
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		//emitter.Emit(Vector3.zero, DenPoint, 0.2, 2, Color.yellow);
+		//particleToDen.
+	}
+}
