aa58e0652be575e0d90b8087b03a4d6d4ecb35f1
Edgar Castro <edzed402@gmail.com>
2016-02-16 13:25:37 -0500

    Added Camera Trigger script. Allows camera zooms etc when player touches certain areas in level.

---
+++ Assets/Scripts/CameraTrigger.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraTrigger : MonoBehaviour {
+	
+	public float cameraResize;
+	public Transform cameraAnchor;
+	public delegate void CameraOverrideEnter (float resize, Transform target = null);
+	public static event CameraOverrideEnter OnCameraOverrideEnter;
+	public delegate void CameraOverrideExit ();
+	public static event CameraOverrideExit OnCameraOverrideExit;
+	
+	void OnTriggerEnter2D (Collider2D hit) {
+		if (hit.CompareTag ("Player")) {
+			if (OnCameraOverrideEnter != null) OnCameraOverrideEnter (cameraResize, cameraAnchor);
+		}
+	}
+	void OnTriggerExit2D (Collider2D hit) {
+		if (hit.CompareTag ("Player")) {
+			if (OnCameraOverrideEnter != null) OnCameraOverrideExit ();
+		}
+	}
+}
